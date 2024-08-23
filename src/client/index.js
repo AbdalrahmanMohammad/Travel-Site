db1cb6d746c88c6854adf745b157e1add1519dd6
Abdalrahman-22 <abdalrahman.s.mohammad@gmail.com>
2024-08-23 22:06:18 +0300

    remove old sass files

--- src/client/index.js
+++ src/client/index.js
@@ -5,8 +5,6 @@ import { checkForUrl } from './js/urlChecker'
 // sass files
 import './styles/resets.scss'
 import './styles/base.scss'
-import './styles/footer.scss'
-import './styles/form.scss'
 import './styles/header.scss'
 
 
